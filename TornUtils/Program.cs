be2cf5e965ebc6491237812b08fe6be19a2b23cd
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-11-30 11:19:04 +0100

    install Microsoft.AspNet.WebApi.Client

--- TornUtils/Program.cs
+++ TornUtils/Program.cs
@@ -1,13 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-namespace TornUtils
+﻿namespace TornUtils
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+               
         }
     }
 } 
