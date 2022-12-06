25771bc25e37944ce7ebdc6bd5e992d1c86cd306
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-12-06 13:51:19 +0100

    move api request to TornAPI class

--- TornUtils/Program.cs
+++ TornUtils/Program.cs
@@ -1,43 +1,27 @@
 ﻿using System.Net.Http.Headers;
+using System.Data.SqlClient;
+using System.Text;
 
 namespace TornUtils
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            InvokeRequestResponseService().Wait();
-        }
-
-        static async Task InvokeRequestResponseService()
-        {
-            var handler = new HttpClientHandler()
+            var tornAPI = new TornAPI();
+            HttpResponseMessage response = await tornAPI.Query("janepe", "attacks");
+            if (response.IsSuccessStatusCode)
             {
-                ClientCertificateOptions = ClientCertificateOption.Manual,
-                ServerCertificateCustomValidationCallback = (HttpRequestMessage, cert, cetChain, policyErrors) => { return true; }
-            };
-            using (var client = new HttpClient(handler))
-            {
-                string? key = System.Environment.GetEnvironmentVariable("TORN_API_KEY");
-                client.BaseAddress = new Uri("https://api.torn.com");
-
-                string user = "janepe";
-                string selections = "attacks";
-                HttpResponseMessage response = await client.GetAsync($"/user/{user}?selections={selections}&key={key}");
-                if (response.IsSuccessStatusCode)
-                {
-                    string result = await response.Content.ReadAsStringAsync();
-                    Console.WriteLine(result);
-                }
-                else
-                {
-                    Console.WriteLine($"The request failed with status code: {response.StatusCode}");
-                    Console.WriteLine(response.Headers.ToString());
-                    var content = response.Content.ReadAsStringAsync();
-                    Console.WriteLine(content);
-                }
+                string result = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(result);
             }
-
+            else
+            {
+                Console.WriteLine($"The request failed with status code: {response.StatusCode}");
+                Console.WriteLine(response.Headers.ToString());
+                var content = response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }            
         }
     }
 }
---
+++ TornUtils/TornAPI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TornUtils;
+internal class TornAPI
+{
+    public Uri baseAddress = new Uri("https://api.torn.com");
+
+    private HttpClientHandler handler;
+    private string key;
+
+    public TornAPI()
+    {
+        // setup configuration and credentials from environment variables
+        key = System.Environment.GetEnvironmentVariable("TORN_API_KEY") 
+            ?? throw new ConfigError("Set TORN_API_KEY environment variable.");
+
+        handler = new HttpClientHandler()
+        {
+            ClientCertificateOptions = ClientCertificateOption.Manual,
+            ServerCertificateCustomValidationCallback = (HttpRequestMessage, cert, cetChain, policyErrors) => { return true; }
+        };
+    }
+
+    public async Task<HttpResponseMessage> Query(string user, string selections)
+    {
+        using (var client = new HttpClient(handler))
+        {
+            client.BaseAddress = baseAddress;
+            HttpResponseMessage response = await client.GetAsync(
+                $"/user/{user}?selections={selections}&key={key}");
+            return response;
+        }
+    }
+}
+
+public class ConfigError : Exception
+{
+    public ConfigError(string s) : base(s) { }
+}
