2c5f41db3c62332fd23b90dc681f567a48805adb
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-12-06 10:28:49 +0100

    SQL database connector

---
+++ TornUtils/Database.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TornUtils;
+internal class Database
+{
+    public static SqlConnection GetDatabaseConnection()
+    {
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+        
+        builder.DataSource = System.Environment.GetEnvironmentVariable("DB_DATASOURCE");
+        builder.UserID = System.Environment.GetEnvironmentVariable("DB_USERID");
+        builder.Password = System.Environment.GetEnvironmentVariable("DB_PASSWORD");
+        builder.InitialCatalog = System.Environment.GetEnvironmentVariable("DB_INITIALCATALOG");
+
+        return new SqlConnection(builder.ConnectionString);
+    }
+}
